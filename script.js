431437ea6ddcfe809cb91b2ad6f4674a20400ca3
Emily Wirtz <ewirtz3@gmail.com>
2020-04-13 22:16:33 -0500

    pseudo coding js

---
+++ script.js
@@ -0,0 +1,5 @@
+//user searches for a city, upon click of spyglass use AJAX to query that city from weather API
+//today's forecast populates to the forecast-today div (card), 5-day forecast populates to the forecast-future(cards)
+//also upon click of spyglass, city saves in localStorage
+//upon refresh, any cities in localStorage persist/append to unordered list (id: past-cities), need to create <li class="list-group-item"> for each
+//upon refresh, last searched city's forecast-today and forecast-future persists
